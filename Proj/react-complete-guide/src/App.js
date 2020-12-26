bd07d2434be919811736829ec7c200d96a5aab9f
Vatan Soni <vatan.soni.ext@sivantos.com>
2020-12-27 00:02:40 +0530

    Using the useState() Hook for State Manipulation

--- Proj/react-complete-guide/src/App.js
+++ Proj/react-complete-guide/src/App.js
@@ -1,49 +1,56 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Person from "./Person/Person";
 
-class App extends Component {
-  state = {
+const app = () => {
+  const [personState, setPersonState] = useState({
     persons: [
       { name: "vatan", age: 20 },
       { name: "tanuja", age: 15 },
     ],
-  };
-  switchNamehandler = () => {
-    this.setState({
+  });
+
+  const [otherState, setOtherState] = useState({
+    otherName: "Third Party"
+  });
+
+  console.log(personState, otherState);
+
+  const personStateswitchNamehandler = () => {
+    setPersonState({
       persons: [
         { name: "vatan", age: 20 },
         { name: "tanuja soni", age: 15 },
       ],
+      otherProp: "Hii",
     });
   };
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={this.switchNamehandler}>Click!!!</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        >
-          Cricket, Badminton
-        </Person>
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        >
-          Chess
-        </Person>
-      </div>
-    );
-  }
-}
 
-export default App;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+      <button onClick={personStateswitchNamehandler}>Click!!!</button>
+      <Person
+        name={personState.persons[0].name}
+        age={personState.persons[0].age}
+      >
+        Cricket, Badminton
+      </Person>
+      <Person
+        name={personState.persons[1].name}
+        age={personState.persons[1].age}
+      >
+        Chess
+      </Person>
+    </div>
+  );
+};
+
+export default app;
