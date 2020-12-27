e56861f65bac801b22c6061f36b00df7fd96a284
Vatan Soni <vatan.soni.ext@sivantos.com>
2020-12-27 23:03:48 +0530

    passing method ref, Two way binding, Stylesheet, Inline styles

--- Proj/react-complete-guide/src/App.js
+++ Proj/react-complete-guide/src/App.js
@@ -1,56 +1,76 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Person from "./Person/Person";
 
-const app = () => {
-  const [personState, setPersonState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: "vatan", age: 20 },
       { name: "tanuja", age: 15 },
     ],
-  });
-
-  const [otherState, setOtherState] = useState({
-    otherName: "Third Party"
-  });
-
-  console.log(personState, otherState);
-
-  const personStateswitchNamehandler = () => {
-    setPersonState({
+  };
+  
+  //If we dont use function variable then in defination we will not be able to use 'this'
+  switchNamehandler = (newName) => {
+    this.setState({
       persons: [
-        { name: "vatan", age: 20 },
+        { name: newName, age: 20 },
         { name: "tanuja soni", age: 15 },
       ],
-      otherProp: "Hii",
     });
   };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-      <button onClick={personStateswitchNamehandler}>Click!!!</button>
-      <Person
-        name={personState.persons[0].name}
-        age={personState.persons[0].age}
-      >
-        Cricket, Badminton
-      </Person>
-      <Person
-        name={personState.persons[1].name}
-        age={personState.persons[1].age}
-      >
-        Chess
-      </Person>
-    </div>
-  );
-};
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: event.target.value, age: 20 },
+        { name: "tanuja soni", age: 15 },
+      ],
+    });
+  };
 
-export default app;
+  
+
+  render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font:'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button style={style} onClick={this.switchNamehandler.bind(this,"Button Soni")}>Click!!!</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          click = {this.switchNamehandler.bind(this,"Vatan Soni")}
+          changed = {this.nameChangedHandler}
+        >
+          Cricket, Badminton
+        </Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click = {this.switchNamehandler.bind(this,"Tanuja Soni")}
+          changed = {this.nameChangedHandler}
+        >
+          Chess
+        </Person>
+      </div>
+    );
+  }
+}
+
+export default App;
