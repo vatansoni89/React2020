2889a29d06c539c0e9938c7c706a4c8285eb3a76
Vatan Soni <vatan.soni.ext@sivantos.com>
2020-12-29 21:16:29 +0530

    Restructuring project

--- Proj/react-complete-lists/src/containers/App.js
+++ Proj/react-complete-lists/src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import logo from "../logo.svg";
 import "./App.css";
-import Person from "./Person/Person";
+import Person from "../components/Persons/Person/Person";
 
 class App extends Component {
   state = {
